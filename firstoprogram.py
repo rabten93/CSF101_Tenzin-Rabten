258d11002fe9bca25b97b39be3e65b6217ac56a4
rabten93 <tenzinrabten592@gmail.com>
2025-08-12 16:23:05 +0530

    Add files via upload

--- firstoprogram.py
+++ firstoprogram.py
@@ -1,2 +1,9 @@
 print("Hello World")
 print("CST")
+a=2
+b=3
+sum=a+b
+print("the sum of a and b is: ",sum)
+name=input("Enter your name: ")
+age=input("Enter your age: ")
+print("you are",name ,"and you are", age ,"years old")
